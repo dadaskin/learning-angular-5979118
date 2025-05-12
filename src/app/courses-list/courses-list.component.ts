fadd2d6299b198287cb830516554cce36eb77da0
adaskind <davidadaskin@comcast.net>
2025-05-12 09:27:57 -0700

    Add property binding

--- src/app/courses-list/courses-list.component.ts
+++ src/app/courses-list/courses-list.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './courses-list.component.css'
 })
 export class CoursesListComponent {
-
+  title: string = "Available Stuff";
+  courses = [
+    { id:1, title: 'Into to Ang.', description: 'Basics', price: 49, date: '2025-03-01', soldOut: false, img: 'angular-logo.png' },
+    { id:2, title: 'Adv. Ang.', description: 'More', price: 99, date: '2024-04-01', soldOut: true, img: 'angular-logo.png' },
+  ];
 }
