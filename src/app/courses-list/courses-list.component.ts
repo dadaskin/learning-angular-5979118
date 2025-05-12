f22fddb5f808ae2b9d11575f854664c064450c5a
adaskind <davidadaskin@comcast.net>
2025-05-12 10:06:44 -0700

    Grab the ngOnInit() hook

--- src/app/courses-list/courses-list.component.ts
+++ src/app/courses-list/courses-list.component.ts
@@ -1,5 +1,5 @@
 import { NgStyle } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-courses-list',
@@ -7,12 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './courses-list.component.html',
   styleUrl: './courses-list.component.css'
 })
-export class CoursesListComponent {
+export class CoursesListComponent implements OnInit {
   title: string = "Available courses";
   courses = [
     { id: 1, title: 'Intro to Angular', description: 'Learn the basics of Angular', price: 49, date: '2025-03-01', soldOut: false, img: 'angular-logo.png' },
     { id: 2, title: 'Advanced Angular', description: 'Deep dive into Angular internals', price: 99, date: '2025-04-10', soldOut: true, img: 'angular-logo.png' },
   ];
+
+  ngOnInit(): void {
+    console.log("Courseslist initialized")
+  }
 
   viewDetails(title: string): void {
     alert(`Viewing details for ${title}`)
